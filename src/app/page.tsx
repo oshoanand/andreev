08d9e15a3b250288ad4a4b5653ba61d1b5b5356d
osho ved <oshoaved@gmail.com>
2025-06-03 16:49:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection>
    
    with the text content:
    
        Discover Your Next FavoriteExplore our curated collection of high-quality products, designed to inspire and delight your everyday life.Shop Collection
    
    Relevant files:
    
        - https://6000-firebase-studio-1748687566085.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_next_d246b0c1._.js
        - https://6000-firebase-studio-1748687566085.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1748687566085.cluster-ux5mmlia3zhhask7riihruxydo.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    use sliding animation fade in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,8 +117,9 @@ const HeroSection = () => {
           animate="center"
           exit="exit"
           transition={{
-            x: { type: "spring", stiffness: 300, damping: 30 },
-            opacity: { duration: 0.2 }
+            type: "spring",
+            stiffness: 300,
+            damping: 30
           }}
           drag="x"
           dragConstraints={{ left: 0, right: 0 }}
